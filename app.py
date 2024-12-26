3af455dac3b7902ebee1fb8a4b7365e63e509fb4
fhtozetto <fhtozetto@gmail.com>
2024-12-26 20:35:42 -0300

    Receita por estado em barras

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 from dataset import df
 from utils import format_number
-from graficos import grafico_map_estado, grafico_rec_mensal
+from graficos import grafico_map_estado, grafico_rec_mensal, grafico_rec_estado
 
 st.set_page_config(layout='wide')
 st.title("Dashboard de Vendas :shopping_trolley:")
@@ -15,6 +15,7 @@ with aba2:
     with coluna1:
         st.metric('Receita Total', format_number(df['Preço'].sum(), 'R$'))
         st.plotly_chart(grafico_map_estado, use_container_width=True)
+        st.plotly_chart(grafico_rec_estado, use_container_width=True)
     with coluna2:
         st.metric('Quantidade de vendas', format_number(df.shape[0]))
         st.plotly_chart(grafico_rec_mensal, use_container_width=True)
--- graficos.py
+++ graficos.py
@@ -5,11 +5,11 @@ grafico_map_estado = px.scatter_geo(
     df_rec_estado, 
     lat = 'lat',
     lon = 'lon',
-    scope = 'south america',
+    scope = 'south america', # Limita o mapa para América do Sul
     size = 'Preço',
-    template = 'seaborn',
+    template = 'seaborn', # Usa um tema de plotly express
     hover_name = 'Local da compra',
-    hover_data = {'lat': False, 'lon': False},
+    hover_data = {'lat': False, 'lon': False}, # Não exibe as coordenadas
     title = 'Receita por Estado'
 )
 
@@ -17,11 +17,19 @@ grafico_rec_mensal = px.line(
     df_rec_mensal,
     x = 'Mes',
     y = 'Preço',
-    markers = True,
-    range_y = (0, df_rec_mensal.max()),
-    color = 'Ano',
+    markers = True, # Adiciona marcadores
+    range_y = (0, df_rec_mensal.max()), # Limita o eixo Y
+    color = 'Ano', # Define a cor do gráfico por ano
     line_dash = 'Ano',
     title = 'Receita Mensal'   
 )
 
 grafico_rec_mensal.update_layout(yaxis_title = 'Receita')
+
+grafico_rec_estado = px.bar(
+    df_rec_estado.head(7),
+    x = 'Local da compra',
+    y = 'Preço',
+    text_auto= True, # Exibe o valor no topo dos barras
+    title = 'Top receita por estado' 
+)
